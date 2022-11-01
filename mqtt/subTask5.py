13046ce70d27c5f411108e1162219d06e13556a6
Daniil <69759444+topspeshik@users.noreply.github.com>
2022-11-01 22:56:12 +0800

    Update subTask5.py

--- mqtt/subTask5.py
+++ mqtt/subTask5.py
@@ -27,8 +27,6 @@ def task5(client, userdata, message):
         min_value = data
     
     if topic == "lab/4881/photo/instant":
-        print("hui")
-        
         if data > (min_value + max_value) / 2:
 
             ser.write(bytearray([1]))
