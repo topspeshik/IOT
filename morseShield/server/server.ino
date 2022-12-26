0d1cc5240632ad99c109ed04e6e650e6412c3a54
Daniil <69759444+topspeshik@users.noreply.github.com>
2022-12-26 17:27:26 +0800

    Update server.ino

--- morseShield/server/server.ino
+++ morseShield/server/server.ino
@@ -1,8 +1,8 @@
 #define DATA_PIN 7
 #define DATA_LEVEL LOW
-String ENCODED[] = {".-", "--.."};
-char LETTERS[] = {'A', 'Z'};
-int n_letter = 2;
+
+String ENCODED[] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--..", " "};
+char LETTERS[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '};
 int TU = 1000;
 
 void setup() {
@@ -14,7 +14,7 @@ void setup() {
 void loop() {
   if (Serial.available() >= 1){
     char letter = Serial.read();
-    for (int iletter = 0; iletter < n_letter; iletter++){
+    for (int iletter = 0; iletter < 27; iletter++){
       if (letter == LETTERS[iletter]){
         encode(ENCODED[iletter]);
       }
@@ -24,6 +24,7 @@ void loop() {
 
 void encode(String dashdot){
   int len = dashdot.length();
+  
   for (int isymbol = 0; isymbol < len; isymbol++){
     if (dashdot[isymbol] == '.'){
       digitalWrite(DATA_PIN, DATA_LEVEL);
@@ -35,8 +36,12 @@ void encode(String dashdot){
       delay(3*TU);
       digitalWrite(DATA_PIN, !DATA_LEVEL);
     }
+    else if (dashdot[isymbol] == ' '){
+      delay(TU);
+    }
+    
     delay(TU);
   }
   delay(2 * TU);
-}
 
+}
