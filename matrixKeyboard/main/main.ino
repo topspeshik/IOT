9c1a1b7fefad69d00f3d1788f8a0ff4ec8ebb385
Daniil <69759444+topspeshik@users.noreply.github.com>
2023-02-14 17:26:33 +0800

    Update main.ino

--- matrixKeyboard/main/main.ino
+++ matrixKeyboard/main/main.ino
@@ -2,9 +2,6 @@
 #define pin4 4
 #define pin5 5
 #define pin6 6
-int d3 = 0;
-int d4 = 0;
-String result;
 
 void setup() {
   Serial.begin(9600);
@@ -14,41 +11,41 @@ void setup() {
   pinMode(pin6, OUTPUT);
 }
 
+int p3 = 0;
+int p4 = 0;
+String buttons; 
+
 void loop() {
   pinMode(pin5, OUTPUT);
   pinMode(pin6, INPUT);
-  result = "";
+  digitalWrite(pin6, LOW);
   digitalWrite(pin5, HIGH);
-
-  d3 = digitalRead(pin3);
-  d4 = digitalRead(pin4);
+  p3 = digitalRead(pin3);
+  p4 = digitalRead(pin4);
   
-  if (d3 == 1 && d4 == 1) {
-    result = result + "12";
-  }
-  else if (d3 == 1 && d4 == 0) {
-    result = result + "1";
-  }
-  else if (d3 == 0 && d4 == 1) {
-    result = result + "2";
-  }
+  buttons = "";
   
-  pinMode(pin6, OUTPUT);
+  if (p3 == 0 && p4 == 1)
+    buttons += "2";
+  else if (p3 == 1 && p4 == 0) 
+    buttons += "1";
+  else if (p3 == 1 && p4 == 1)
+    buttons += "12";
+    
   pinMode(pin5, INPUT);
+  pinMode(pin6, OUTPUT);
+  digitalWrite(pin5, LOW);
   digitalWrite(pin6, HIGH);
   
-  d3 = digitalRead(pin3);
-  d4 = digitalRead(pin4);
+  p3 = digitalRead(pin3);
+  p4 = digitalRead(pin4);
   
-  if (d3 == 1 && d4 == 1) {
-    result = result + "34";
-  }
-  else if (d3 == 1 && d4 == 0) {
-    result = result + "3";
-  }
-  else if (d3 == 0 && d4 == 1) {
-    result = result + "4";
-  }
-  Serial.println(result);
-
+  if (p3 == 0 && p4 == 1) 
+    buttons += "4";
+  else if (p3 == 1 && p4 == 0) 
+    buttons += "3";
+  else if (p3 == 1 && p4 == 1) 
+    buttons += "34";
+  
+  Serial.println(buttons);
 }
